366ae88d1fdad4c6b359f6b4de3e50552208c237
furkankinli <furkan.kinli@ozu.edu.tr>
2015-12-14 22:20:11 +0200

    Update config.php
    seperating connection and queries

--- config.php
+++ config.php
@@ -1,16 +1,5 @@
 <?php
-	$db_name="vote_project"
-
-	$connect=mysql_connect("localhost", "root", "karateke23") or die
-			("Could not connect to database!");
-
-	mysql_select_db($db_name,$connect) or die ("Could not connect to database!);
-
-	mysql_query("SET NAMES utf8");
-	mysql_query("SET CHARACTER SET utf8");
-	mysql_query("SET COLLATION_CONNECTION= utf8_general_ci ");
-
-	include ("vtbaglanti.php");!!!!!!!!!!!!!!!
+	include ("vtbaglanti.php");
 
 	$email=$_POST["email"];
 	$sifre=$_POST["sifre"];
