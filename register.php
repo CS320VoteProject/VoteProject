312b28b5f08ae996d6d52335bbc0710473a2080f
furkankinli <furkan.kinli@ozu.edu.tr>
2015-12-16 01:08:07 +0200

    Update and rename register.html to register.php

---
+++ register.php
@@ -0,0 +1,46 @@
+
+
+<html>
+	<head>
+		<script type="text/javascript" src="jquery-1.9.1.min.js"></script>
+		<link type="text/css" rel="stylesheet" href="popupbox.css"/>
+		<link type="text/css" rel="stylesheet" href="main.css"/>
+	    <script type="text/javascript" src="https://www.google.com/jsapi">	  
+	</script>
+	</head>
+	<body>
+	<div id="logo">
+			<div id="left" style="width: 500px;
+				height: 100px; margin: 10px 15px 10px 8px;">
+				<a href="index.php">
+					<img src="images/logo.png">
+				</a>
+			<div id="right">
+				
+			</div>
+		</div>
+
+		<div id="login">
+			<div id="triangle"></div>
+			<h1>Register</h1>
+				<form action="config.php" method="post">
+					<input type="email" name="email" placeholder="Email" />
+					<input type="text" name="username" placeholder="Username" />
+					<input type="password" name="password" placeholder="Password" />
+					<input type="submit" name="submit" value="Register"/>
+				</form>
+		</div>
+		</br></br></br></br></br></br></br></br></br></br></br></br></br></br>
+		<div id="contacts">
+			<footer id="footer">
+				<ul class="icons">
+					<li>
+						<a href="https://twitter.com/voteproject"> <img src="images/twitter.svg"></a>
+						<a href="https://facebook.com/voteproject"> <img src="images/facebook.svg"></a>
+						<a href="https://github.com/CS320VoteProject/VoteProject"> <img src="images/github.svg"></a>
+					</li>
+				</ul>
+			</footer>
+		</div>
+</body>
+</html>
