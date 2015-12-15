8a2bae82fbb3662c74bf81adf244cf953d87a098
furkankinli <furkan.kinli@ozu.edu.tr>
2015-12-15 22:00:51 +0200

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,11 @@
-<?php
-$db_name="vote_project"
+	<?php
+	$db_name="vote_project";
+	define("DB_PASSWORD", null);
+		$connect=mysql_connect("localhost", "root", null) or die
+				("Could not connect to database!");
 
-	$connect=mysql_connect("localhost", "root", "karateke23") or die
-			("Could not connect to database!");
+		mysql_select_db($db_name,$connect) or die ("Could not connect to database!");
 
-	mysql_select_db($db_name,$connect) or die ("Could not connect to database!);
-
-	mysql_query("SET NAMES utf8");
-	mysql_query("SET CHARACTER SET utf8");
-	mysql_query("SET COLLATION_CONNECTION= utf8_general_ci ");
-?>
+		
+		
+	?>
